95994952b11321431be07a2bf8ca89f4fa6df6c2
JureNajjaci123 <103893893+JureNajjaci123@users.noreply.github.com>
2024-05-02 20:50:43 +0200

    Added new files

---
+++ database/config.php
@@ -0,0 +1,8 @@
+<?php
+//database configurations
+define("DB_HOST","sql105.infinityfree.com");
+define("DB_UNAME","if0_36474124");
+define("DB_PASS","lduAVlpmuGjq");
+define("DB_DNAME","if0_36474124_XXX");
+$conn=mysqli_connect(DB_HOST,DB_UNAME,DB_PASS,DB_DNAME);
+?>
